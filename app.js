458b547f0470440f0e38884e291e8377ed6d0860
LeeHyungGeun <lhg4031@gmail.com>
2013-07-06 15:56:46 +0900

    Angular, Node.js and MongoDB integration

---
+++ app.js
@@ -0,0 +1,94 @@
+'use strict';
+var app = angular.module('app', []);
+
+app.config(['$httpProvider', function($httpProvider){
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
+
+// Factory
+app.factory('socket', function($rootScope){
+    var socket = io.connect();
+    return{
+        on: function(eventName, callback){
+            socket.on(eventName, function(){
+                var args = arguments;
+                $rootScope.$apply(function(){
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback){
+            socket.emit(eventName, data, function(){
+                var args = arguments;
+                $rootScope.$apply(function(){
+                    if(callback){
+                        callback.apply(socket.args);
+                    }
+                });
+            })
+        }
+    };
+});
+
+// Controller
+function UserListCtrl($scope, socket){
+    $scope.showEdit = false;
+    $scope.showLabel = true;
+
+    /* CRUD */
+    //insert
+    $scope.save = function(){
+        var formData = {
+            "username": this.username,
+            "password": this.password,
+            "email": this.email
+        };
+        this.username = '';
+        this.password = '';
+        this.email = '';
+
+        var data = 'data=' + JSON.stringify(formData);
+        socket.emit('insertUser', formData);
+        return false;
+    };
+    //update
+    $scope.update = function(){
+        var userData = {
+            "username": this.user.username,
+            "password": this.user.password,
+            "email": this.user.email,
+            "_id": this.user._id
+        };
+        var data = 'data=' + JSON.stringify(userData);
+        socket.emit('updateUser', userData);
+        return false;
+    };
+    //delete
+    $scope.remove = function(){
+        var userData = {
+            "username": this.user.username,
+            "password": this.user.password,
+            "email": this.user.email
+        };
+        var data = 'data=' + JSON.stringify(userData);
+
+        socket.emit('removeUser', userData);
+        return false;
+    };
+    //get
+    socket.on('getUsers', function(users){
+        $scope.users = users;
+        console.log(users);
+    });
+
+    /* event */
+    $scope.selectUser = function(){
+        if(this.showEdit === false){
+            this.showEdit = true;
+            this.showLabel = false;
+        }
+    };
+
+    socket.emit('getUsers');
+}
